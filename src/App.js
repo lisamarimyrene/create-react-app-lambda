fedf6c05b130072fe939eb3a66a5143366e6713a
lisamarimyrene <lisamy@stud.ntnu.no>
2023-03-13 11:54:10 +0100

    initial changes

--- src/App.js
+++ src/App.js
@@ -36,9 +36,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>Hi, my name is Lisa!</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This is a flowerful website.
           </p>
           <LambdaDemo />
         </header>
